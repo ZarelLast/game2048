3c131031d7bc92155b9d676798efd3a55407910a
Zarel <lastzarel@gmail.com>
2019-10-17 11:39:18 +0700

    Updates

---
+++ precache-manifest.caac4a8ab6f35f92f497c7c9965f4e46.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6205532308722c6a14b5391a8c967151",
+    "url": "/game2048/index.html"
+  },
+  {
+    "revision": "84e859bd12f76ba5c5bc",
+    "url": "/game2048/static/css/main.d2a80172.chunk.css"
+  },
+  {
+    "revision": "f9d1e7a5991f89d7e045",
+    "url": "/game2048/static/js/2.c40a673a.chunk.js"
+  },
+  {
+    "revision": "84e859bd12f76ba5c5bc",
+    "url": "/game2048/static/js/main.30a72cdb.chunk.js"
+  },
+  {
+    "revision": "4e7e9dfe585fcd01a173",
+    "url": "/game2048/static/js/runtime-main.8c8f9080.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/game2048/static/media/logo.25bf045c.svg"
+  }
+]);
