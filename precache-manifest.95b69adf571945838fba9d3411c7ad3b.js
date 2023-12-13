53bf1a39da6ded0d5c50dc72a0b07aaad7b373fb
Muhammad Ilham Triwibowo <39546259+ZarelLast@users.noreply.github.com>
2023-12-14 05:12:46 +0700

    Update precache-manifest.95b69adf571945838fba9d3411c7ad3b.js

--- precache-manifest.95b69adf571945838fba9d3411c7ad3b.js
+++ precache-manifest.95b69adf571945838fba9d3411c7ad3b.js
@@ -1,26 +1,26 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
     "revision": "8e35fb5e68b84618ac8247578afc8772",
-    "url": "/game2048/index.html"
+    "url": "/index.html"
   },
   {
     "revision": "77258c0be06982817533",
-    "url": "/game2048/static/css/main.66103573.chunk.css"
+    "url": "/static/css/main.66103573.chunk.css"
   },
   {
     "revision": "f9d1e7a5991f89d7e045",
-    "url": "/game2048/static/js/2.c40a673a.chunk.js"
+    "url": "/static/js/2.c40a673a.chunk.js"
   },
   {
     "revision": "77258c0be06982817533",
-    "url": "/game2048/static/js/main.f3901ebc.chunk.js"
+    "url": "/static/js/main.f3901ebc.chunk.js"
   },
   {
     "revision": "4e7e9dfe585fcd01a173",
-    "url": "/game2048/static/js/runtime-main.8c8f9080.js"
+    "url": "/static/js/runtime-main.8c8f9080.js"
   },
   {
     "revision": "25bf045ca257e971124f3997d89f321c",
-    "url": "/game2048/static/media/logo.25bf045c.svg"
+    "url": "/static/media/logo.25bf045c.svg"
   }
 ]);
